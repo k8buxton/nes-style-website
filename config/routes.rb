a782a84d117f11dda6de39e704fa5820e69c8fba
Kate Buxton <k8hunter@hotmail.com>
2019-11-11 17:51:27 +0000

    copied website

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources "contacts", only: [:new, :create]
+  get 'about', to: 'pages#about', as: :about
+  get 'design-studio', to: 'pages#design-studio', as: :design_studio
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
